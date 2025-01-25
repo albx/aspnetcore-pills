c770003dbad535f1730d08e7eb55e16d50f243be
albx <albi.mo87@hotmail.it>
2025-01-25 16:31:17 +0100

    episode 2

--- AspNetCorePills.Web/Program.cs
+++ AspNetCorePills.Web/Program.cs
@@ -2,10 +2,41 @@ var builder = WebApplication.CreateBuilder(args);
 
 //Servizi
 
+//Singleton
+//builder.Services.AddSingleton<MyService>();
+
+//Scoped
+//builder.Services.AddScoped<MyService>();
+
+//Transient
+//builder.Services.AddTransient<MyService>();
+
+//Keyed service
+builder.Services.AddKeyedScoped<MyService>("service");
+
 var app = builder.Build();
 
 //Middleware
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet(
+    "/", 
+    ([FromKeyedServices("service")]MyService service) =>
+    {
+        return $"Hello World! {service.GetValue()}";
+    });
 
 app.Run();
+
+#region Service
+class MyService
+{
+    private static int value = 0;
+
+    public MyService()
+    {
+        value++;
+    }
+
+    public int GetValue() => value;
+}
+#endregion
